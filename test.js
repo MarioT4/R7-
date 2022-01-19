dd12b61245a95f07c8f14a5f25a5f0914c15bbd5
Mario Toscano <90327387+MarioT4@users.noreply.github.com>
2022-01-19 14:56:26 +0000

    w

--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 let table;
 let index = 0;
 let dx;
+let indicator = 1;
 function preload() {
   table = loadTable('data.csv', 'csv', 'header');
 }
@@ -14,7 +15,7 @@ function setup() {
 }
 
 function draw() {
-  background(220)
+  background('Gainsboro')
   fill('white')
   stroke('black')
   textSize(20)
@@ -30,12 +31,27 @@ function draw() {
 }
 
 function keyPressed() {
+  if (key === '+') {
   index += 7;
   redraw();
+  }
+   if (key === '-') {
+  index -= 7;
+  redraw();
+  }
+   if (key === '1') {
+  indicator = 0;
+  redraw();
+  }
+if (key === '2') {
+  indicator = 1;
+   redraw();
+  }
 }
 
 function drawWeek(startingRow) {
   //Mr Oswald took it from a bunch of different loops to function drawWeek
+  if (indicator == 0) {
   for (let i = 0; i < 7; i++) {
     const recordMin = table.getNum(startingRow + i, 'record_min_temp');
     const recordMax = table.getNum(startingRow + i, 'record_max_temp');
@@ -73,52 +89,51 @@ function drawWeek(startingRow) {
      text(recordMax,x,100);
      text(recordMin,x,height);
   }
-  //  for (let u = startingRow; u < startingRow + 7; u++) {
-  //    const name = table.getString(u, 'actual_max_temp');
-  //    const actual_max_temp = table.getNum(u, 'actual_max_temp');
-  //     const x =  random(20, 520);
-  //    const y = random(100,370);
-  //    const size = map(actual_max_temp/2, 0, 28, 0, 10)
-  //    const fillColor = map(actual_max_temp, 256,0 ,0, 255)
-  //    fill (fillColor,1,50)
-  //    circle(x,y, actual_max_temp/4)
-  //    fill(0)
-  //    text(name,x,y+ size/2+20)
-  //  }
-  //  for (let o = 0; o < table.getRowCount(); o++) {
-  //    const name = table.getString(o, 'actual_min_temp');
-  //    const actual_min_temp = table.getNum(o, 'actual_min_temp');
-  //     const x =  random(20, 520);
-  //    const y = random(100,370);
-  //    const size = map(actual_min_temp/2, 0, 28, 0, 10)
-  //    const fillColor = map(actual_min_temp, 0,0 ,256, 0)
-  //    fill (fillColor,250,250)
-  //    circle(x,y, actual_min_temp/4)
-  //    fill(0)
-  //    text(name,x,y+ size/2+20)
-  //  }
-  //  for (let p = 0; p < table.getRowCount(); p++) {
-  //    const name = table.getString(p, 'record_min_temp');
-  //    const record_min_temp= table.getNum(p, 'record_min_temp');
-  //     const x =  random(20, 520);
-  //    const y = random(100, 370);
-  //    const size = map(record_min_temp/2, 0, 28, 0, 10)
-  //    const fillColor = map(record_min_temp, 0,0 ,256, 0)
-  //    fill (fillColor,250,0)
-  //    circle(x,y, record_min_temp/4)
-  //    fill(0)
-  //    text(name,x,y+ size/2+20)
-  //  }
-  //  for (let t = 0; t < table.getRowCount(); t++) {
-  //    const name = table.getString(t, 'record_max_temp');
-  //    const record_max_temp= table.getNum(t, 'record_max_temp');
-  //     const x =  random(20, 520);
-  //    const y = random(100, 370);
-  //    const size = map(record_max_temp/2, 0, 28, 0, 10)
-  //    const fillColor = map(record_max_temp, 0,0 ,201, 0)
-  //    fill (fillColor,200,0)
-  //    circle(x,y, record_max_temp/4)
-  //    fill(0)
-  //    text(name,x,y+ size/2+20)
-  //  }  
+  }
+
+  if (indicator == 1) {
+    fill('Gainsboro')
+    rect(0,0, 10000,10000)
+    fill('white')
+    textSize(40)
+    text('some extra data',width/2,height/13)
+    for (let i = 0; i < 7; i++) {
+    const recordMin = table.getNum(startingRow + i, 'record_min_temp');
+    const recordMax = table.getNum(startingRow + i, 'record_max_temp');
+    const actual_mean_temp = table.getNum(startingRow + i, 'actual_mean_temp');
+    const actual_max_temp = table.getNum(startingRow + i, 'actual_max_temp');    
+    const actual_min_temp = table.getNum(startingRow + i, 'actual_min_temp');        
+
+    const x = (i + 1) * dx;
+    let y = map(actual_mean_temp, recordMin, recordMax, height-50, 100);
+    // Fredirck De Blessler and down
+    let size = map(actual_mean_temp / 2, 0, 28, 0, 10);
+    let fillColor = map(actual_mean_temp, recordMin, recordMax, 0, 255)
+    fill (1,fillColor,50)
+    
+    circle(x, y, actual_mean_temp / 4)
+    fill(0)
+    text(actual_mean_temp, x, y + size / 2 + 20)
+    
+    y = map(actual_max_temp, recordMin, recordMax, height-50, 100);
+     size = map(actual_max_temp/2, 0, 28, 0, 10)
+     fillColor = map(actual_max_temp, 256,0 ,0, 255)
+     fill(fillColor, 1, 50)
+     circle(x,y, actual_max_temp/4)
+     fill(0)
+     text(actual_max_temp,x,y+ size/2+20)
+    
+    y = map(actual_min_temp, recordMin, recordMax, height-50, 100);
+     size = map(actual_min_temp/2, 0, 28, 0, 10)
+     fillColor = map(actual_min_temp, 0,0 ,256, 0)
+     fill (250,250,fillColor)
+     circle(x,y, actual_min_temp/4)
+     fill(0)
+     text(actual_min_temp,x,y+ size/2+20)
+
+     text(recordMax,x,100);
+     text(recordMin,x,height);
+  }
+    
+  }
 }
